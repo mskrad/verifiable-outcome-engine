1df29142d5868c574b7b14b62e2257ea4c14213e
mskrad <lilhubba1r@gmail.com>
2026-04-21 23:18:56 +0300

    chore: remove program source from public repo

--- programs/outcome/src/math/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod compiled_outcome_v1;
-pub mod rng_v1;
--- programs/outcome/src/state/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod approved_outcome_artifact;
-pub mod approved_outcome_artifact_chunk;
-pub mod outcome_config;
-pub mod outcome_resolution;
-pub mod program_config;
